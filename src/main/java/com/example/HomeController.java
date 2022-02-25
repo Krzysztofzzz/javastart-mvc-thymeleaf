6ee38ce324bd4b0804026b94501abc27c1e6322c
Krzysztof <krzubrycki@gmail.com>
2022-02-25 23:14:30 +0100

    implemented example files

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    String home(Model model){
+        model.addAttribute("message", "To jest tekst z kontrollera przed zmianą");
+        return "index";
+    }
 }
