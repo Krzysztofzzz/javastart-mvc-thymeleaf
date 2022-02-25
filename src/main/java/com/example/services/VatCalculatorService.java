f1a5d016cb116a897a4d16f494721cf3fd0a8ce2
Krzysztof <krzubrycki@gmail.com>
2022-02-25 21:43:12 +0100

    implemented service

--- src/main/java/com/example/services/VatCalculatorService.java
+++ src/main/java/com/example/services/VatCalculatorService.java
@@ -1,4 +1,22 @@
 package com.example.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class VatCalculatorService {
+
+    public double calculateBruttoPrice(double nettoPrice, String category) {
+        return nettoPrice + calculateVat(nettoPrice, category);
+    }
+
+    private double calculateVat(double nettoPrice, String category) {
+        double vatRate = switch (category) {
+            case "service" -> 0.23;
+            case "food" -> 0.05;
+            default -> 0.15;
+        };
+        return nettoPrice * vatRate;
+    }
+
+
 }
