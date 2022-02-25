f026bed4432f819cc9332bb942a09953f077a7f9
Krzysztof <krzubrycki@gmail.com>
2022-02-25 22:19:44 +0100

    implemented classes and views

--- src/main/java/com/example/ExampleController.java
+++ src/main/java/com/example/ExampleController.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExampleController {
+
+    @GetMapping("/")
+    String example(Model model){
+        model.addAttribute(new Person("Jan","Kowalski",17));
+        return "example";
+    }
 }
