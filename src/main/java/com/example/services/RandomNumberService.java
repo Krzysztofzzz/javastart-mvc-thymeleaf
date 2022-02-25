b2972ba9dedd59ec33394e3bf509b48d4ecbd6e9
Krzysztof <krzubrycki@gmail.com>
2022-02-25 21:12:08 +0100

    implemented Service

--- src/main/java/com/example/services/RandomNumberService.java
+++ src/main/java/com/example/services/RandomNumberService.java
@@ -1,4 +1,16 @@
 package com.example.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
 public class RandomNumberService {
+    private final Random random = new Random();
+
+    int generateRandomInRange(int start, int end) {
+        int range = end - start + 1;
+        int randomNumber = random.nextInt(range);
+        return start + randomNumber;
+    }
 }
